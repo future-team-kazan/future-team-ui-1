28a93a2d765ccac81703ff0fd4189b56a8793823
j.savicheva <j.savicheva@innopolis.ru>
2019-08-10 22:40:04 +0300

    подключение модуля аутентификации - не закончено

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -8,4 +8,26 @@ export const MENU_ITEMS: NbMenuItem[] = [
     link: '/pages/test-page',
     home: true,
   },
+  {
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
 ];
